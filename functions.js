d6e4f5e52da6444459d172da1a7254c8d08affd3
juliocesarn2010@hotmail.com <juliocesarn2010@hotmail.com>
2020-09-01 20:42:23 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,29 @@
+function square(number) {
+    return number * number;
+}
+
+function message() {
+    return "hola mundo.";
+}
+
+function messageTwo() {
+    var message = "hola mundo.";
+}
+
+function messageThree(name, number) {
+    return `hola ${ name }, yo se que tu tienes ${ square(number)} años`;
+}
+
+function showFullName(user) {
+    return `hola ${user.name} ${user.lastName}`;
+}
+
+function showFullNameArray(users) {
+    return `hola ${users[0].name} ${users[0].lastName}`;
+}
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log( sum(2,5) );
