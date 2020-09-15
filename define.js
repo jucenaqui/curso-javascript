513e3695551cbfef6e05ec2026fcca8824e63831
julio cesar naranjo <juliocesarn2010@hotmail.com>
2020-09-15 15:12:37 -0500

    se elimina archivos y se crea define.js

---
+++ define.js
@@ -0,0 +1,26 @@
+/*
+¿Qué es una función?
+Una función es un proceso que toma alguna entrada, llamada argumentos, 
+y produce una salida llamada valor de retorno. 
+Las funciones pueden tener los siguientes propósitos:
+Mapeo: produce una salida basada en entradas dadas. 
+  Una función asigna valores de entrada a valores de salida.
+Procedimientos: se puede llamar a una función para realizar
+  una secuencia de pasos. La secuencia se conoce como procedimiento,
+  y la programación en este estilo se conoce como programación procedimental .
+E/S: existen algunas funciones para comunicarse con otras partes del sistema,
+  como la pantalla, el almacenamiento, los registros del sistema o la red.
+
+ f(x) = 2x
+ Lo que significa que estamos declarando una función llamada f y toma
+  un argumento llamado x y multiplica x por 2.
+ Para usar esta función, simplemente proporcionamos un valor para x:
+ f (2)
+
+
+
+ 
+
+
+   */
+
