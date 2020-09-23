bfdeaf5d00ac961e495f2b0d59d830bc5e0ef18d
julio cesar naranjo <juliocesarn2010@hotmail.com>
2020-09-22 20:54:35 -0500

    se crea archivo borrowing.js

---
+++ essential/borrowing.js
@@ -0,0 +1,16 @@
+// function borrowing
+var person = {
+  name: 'Bruce',
+  lastname: 'Wayne',
+  fullname: function() {
+    return this.name + ' ' + this.lastname;
+  }
+};
+
+var actor = {
+  name: 'Clint',
+  lastname: 'Eastwood'
+};
+
+var result = person.fullname.apply(actor);
+console.log(result); // Clint Eastwood
